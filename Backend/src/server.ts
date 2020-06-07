d18851f172cb3b095c2fe9148a32dfb33249251b
Ilda Neta <iildaemanoely@gmail.com>
2020-06-07 08:27:31 -0300

    feat: inserting the cors configuration and using the uploads of images with static URL

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+// arquivo estáticos de uma pasta específica pdf, image
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
