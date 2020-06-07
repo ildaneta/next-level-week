aa57590dc62273fa4d9ee6711fdb750d4d221869
Ilda Neta <iildaemanoely@gmail.com>
2020-06-07 08:30:52 -0300

    feat: creating the project routes

--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -1,9 +1,21 @@
 import express from 'express';
 
-const routes = express.Router();
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Uhuuu consegui' });
-});
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.post('/points', pointsController.create);
+routes.get('/items', itemsController.index);
+routes.get('/points/:id', pointsController.show);
+routes.get('/points/:id', pointsController.index);
+
+// index se for listagem
+// show se for exibir um item
+// create
+// update
+// delete
 
 export default routes;
