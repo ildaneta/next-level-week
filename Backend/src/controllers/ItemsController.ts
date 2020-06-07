c47f2b726c5fc7dff3d34368f25ba63be9035301
Ilda Neta <iildaemanoely@gmail.com>
2020-06-07 08:31:09 -0300

    feat: creating the items controller

---
+++ Backend/src/controllers/ItemsController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+
+class ItemsController {
+  async index(request: Request, response: Response) {
+    const items = await knex('items').select('*');
+
+    // serialização - transformar os dados do banco em um novo formato que o frontend necessitará
+    const serializedItems = items.map((item) => {
+      return {
+        id: item.id,
+        title: item.title,
+        image_url: `http://192.168.31.122:3333/uploads/${item.image}`,
+      };
+    });
+
+    return response.json(serializedItems);
+  }
+}
+
+export default ItemsController;
