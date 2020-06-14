44c038212050717e740c575ea704aeaedb5ea3e0
Ilda Neta <iildaemanoely@gmail.com>
2020-06-14 19:39:00 -0300

    feat: creating the archives for ecoleta project

--- Frontend/web/.eslintrc.js
+++ Frontend/web/.eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier'
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -16,16 +12,24 @@ module.exports = {
     ecmaVersion: 11,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'prettier'
-  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    "camelcase": "off",
-    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
-    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'prettier/prettier': ['error', { singleQuote: true, parser: 'flow' }],
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.tsx'] },
+    ],
+    settings: {
+      'import/resolver': {
+        typescript: {},
+      },
+    },
+    'global-require': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-expressions': ['error', { allowTaggedTemplates: true }],
   },
 };
--- Frontend/web/src/App.tsx
+++ Frontend/web/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import Routes from './routes';
+
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+  return <Routes />;
 }
 
 export default App;
